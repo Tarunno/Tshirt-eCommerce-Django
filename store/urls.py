afccd82ea95f0214375bd4cf58821a9c9e78caa0
Mahadi Hasan Tarunno <47926003+Tarunno@users.noreply.github.com>
2020-10-13 11:32:41 +0600

    Update urls.py

--- store/urls.py
+++ store/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     path('search/<str:query>', views.search, name='search'),
     path('product/<int:id>/', views.view_product, name='product'),
     path('rating_update/', views.rating_update, name='rating_update'),
-    path('update_review/', views.update_review, name='update_review')
+    path('update_review/', views.update_review, name='update_review'),
+    path('order_placed/', views.order_placed, name="order_placed")
 
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
