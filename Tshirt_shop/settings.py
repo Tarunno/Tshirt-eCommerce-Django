e0601946059306016e745e43d94bd35f6a34af03
Mahadi Hasan Tarunno <47926003+Tarunno@users.noreply.github.com>
2020-10-25 20:15:16 +0600

    Update settings.py

--- Tshirt_shop/settings.py
+++ Tshirt_shop/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'store.apps.StoreConfig',
-    'crispy_forms'
+    'crispy_forms',
+    'django_cleanup.apps.CleanupConfig'
 ]
 
 MIDDLEWARE = [
